1769d3e3a85404d0ff438eb5430a1e71f3aa3c0b
kasyfizamzam <kasyfi.zamzam@gmail.com>
2023-11-04 14:13:13 +0800

    update

--- src/components/authLinks/auth-links.jsx
+++ src/components/authLinks/auth-links.jsx
@@ -1,18 +1,50 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+
 import styles from "./auth-links.module.css";
 
 export const AuthLinks = () => {
-  const status = "authenticated";
+  const status = "unauthenticated";
+
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write">Write</Link>
-          <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "unauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 export const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Kasyfi here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+            placeat tempore eos obcaecati exercitationem. Omnis quasi libero
+            unde suscipit officia! Qui repudiandae, aperiam dicta corrupti in
+            laudantium error consequuntur non.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
